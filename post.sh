d0db428b4e49c6d3a45799f0c4fc291d8f138b95
Shaun McDonnell <sm@eisoft.com>
2018-01-06 19:45:08 -0500

    Removed stub files for testing.

--- post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-logline "Updating OpenWrt feeds."
-${repository_target}/scripts/feeds update -a
-
-logline "Changing permissions on set_cpu_affinit script..."
-chmod -v +x ${repository_target}/files/etc/init.d/set_cpu_affinity
